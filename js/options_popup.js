a70898dc002a4258852e4370d582e209b6794e17
“DavidYaacov” <6254337+DavidYaacov@users.noreply.github.com>
2021-11-05 20:20:46 +0200

    fix overflow-scroll in firefox linux and MacOS. Different media query to detect FireFox. Added back button from donate page. Using Firefox Theme Colors on Add-on.

--- js/options_popup.js
+++ js/options_popup.js
@@ -27,13 +27,42 @@ textElements.forEach((e) => {
 });
 
 let donateButton = document.getElementById('donatebutton');
-
 donateButton.onclick = function () {
-    document.getElementById('settings').remove();
-    donateButton.remove();
+    document.getElementById('settings').style.display = 'none';
+    donateButton.style.display = 'none';
     document.getElementById('donate').style.display = 'block';
+    settingsButton.style.display = 'block';
 }
-
 document.getElementById('paypallink').onclick = function () {
     chrome.tabs.create({url: 'https://paypal.me/pietervanheijningen'})
 }
+
+let settingsButton = document.getElementById('settingsButton');
+settingsButton.onclick = function () {
+    document.getElementById('donate').style.display = 'none';
+    settingsButton.style.display = "none";
+    document.getElementById('settings').style.display = 'block';
+    donateButton.style.display = 'block';
+}
+
+/**
+ * use theme colors from firefox
+ */
+function setFirefoxThemecolors(theme)
+{
+  if (theme.colors != null || theme.colors.toolbar != null & theme.colors.popup != null)
+  {
+    document.getElementById('banner').style.backgroundColor = theme.colors.toolbar 
+    document.getElementById('banner').style.color = theme.colors.toolbar_text
+    
+    document.body.style.backgroundColor = theme.colors.popup
+    document.body.style.color = theme.colors.popup_text
+  }
+}
+
+(async function () {
+	if ((navigator.userAgent.includes = "Firefox")) {
+        var theme = await browser.theme.getCurrent();
+		setFirefoxThemecolors(theme);
+	}
+})();
